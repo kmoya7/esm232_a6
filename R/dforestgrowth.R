26a2c52d9d83bc3d04b729bb72a1d568d7ac70e2
Katheryn Moya <kmoya@ESM.UCSB.EDU>
2023-05-19 14:29:29 -0700

    started assignment 6 - created model, ran for 300 years, and made a plot

---
+++ R/dforestgrowth.R
@@ -0,0 +1,19 @@
+#'  Forest growth model
+#'  Forest size is measured in units of carbon (C)
+#' @param time time since start
+#' @param C forest size in carbon
+#' @param parms - as list with three values, r, K, closure
+#' @param r growth rate
+#' @param K carrying capacity (kg C)
+#' @param g linear growth rate after closure
+#' @param closure canopy closure (kg C)
+#' @return derivative of population with time
+
+dforestgrowth= function(time, C, parms) {
+
+  dC = ifelse(C < parms$closure, parms$r*C, parms$g*(1- C/parms$K))
+  # if size of forest is under canopy closure, then r*C, if not g * (1 - C/K)
+  dC = ifelse(C >= parms$K, 0, dC)
+
+  return(list(dC))
+}
